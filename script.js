4a12f72984a2b7cef2b4d9d4d7afe8b5afe1e281
chato-gpt <mamfilho72@gmail.com>
2025-04-29 10:09:01 -0300

    script.js

--- script.js
+++ script.js
@@ -9,32 +9,13 @@ const frasesDeCarregamento = [
   "Pensando como um gênio...",
   "Formatando respostas...",
   "Consultando o oráculo digital..."
-  "Vai pesquisar no Google, preguiçoso!",
-  "Compra uma Barsa e vai estudar!",
-  "Sério que você precisa de ajuda pra isso?",
-  "Até uma pedra saberia essa resposta.",
-  "Não sou seu professor particular.",
-  "Já pensou em usar o cérebro?",
-  "Não tenho paciência pra isso hoje.",
-  "Você tá de brincadeira comigo, né?",
-  "Pergunta pro seu vizinho, talvez ele saiba.",
-  "Vai estudar e para de me encher!",
-  "Achei que ninguém poderia fazer uma pergunta pior, até você chegar.",
-  "Nem vale a pena responder isso.",
-  "Google tá aí pra isso, mané!",
-  "Isso aí até minha avó sabe!",
-  "Me poupe, se poupe, nos poupe.",
-  "Não sou pago pra te ensinar coisas básicas.",
-  "Que pergunta imbecil, faça outra.",
-  "Cara... sério mesmo que você perguntou isso?",
-  "Tente de novo. Talvez menos vergonha dessa vez.",
-  "Seja útil, vai ler um livro."
 ];
 
 const respostas = [
   /* ... seu array grande de respostas ... */
   "Essa pergunta me pegou de surpresa!",
   "Interessante... muito interessante!",
+  
   // adicione suas 200+ respostas aqui!
 ];
 
